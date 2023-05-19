e3193760796ec2564f364819c62311ad7a922951
mernaadell <mernaadel963@gmail.com>
2023-05-19 13:45:48 -0400

    add users and roles many to many relation

---
+++ src/main/java/com/blogpost/restfullapi/entity/User.java
@@ -0,0 +1,35 @@
+package com.blogpost.restfullapi.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String name;
+    @Column(nullable=false, unique = true)
+    private String username;
+    @Column(nullable=false, unique = true)
+    private String email;
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)//when get user get it's roles too
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> role; //set becz unique
+
+}
